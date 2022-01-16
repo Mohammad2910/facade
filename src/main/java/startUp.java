b21e717d16aeee79a43e83837f0bf1ecb85faac6
Nafiu <s184174@student.dtu.dk>
2022-01-16 14:08:00 +0100

    added dockerfile

--- src/main/java/startUp.java
+++ src/main/java/startUp.java
@@ -1,9 +1,10 @@
+import adapters.FacadeController;
 import messaging.implementations.RabbitMqQueue;
 
 public class startUp {
     public static void main(String[] args) throws Exception {
         System.out.println("startup");
         var mq = new RabbitMqQueue("localhost");
-
+        new FacadeController(mq);
     }
 }
